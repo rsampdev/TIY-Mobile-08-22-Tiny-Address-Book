0e4d5d286b204400f76c250953b1115bf6721622
rsampdev <rsampdev@gmail.com>
2016-08-22 16:03:29 -0400

    Initial Commit

---
+++ Tiny Address Book/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tiny Address Book
+//
+//  Created by Rodney Sampson on 8/22/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
